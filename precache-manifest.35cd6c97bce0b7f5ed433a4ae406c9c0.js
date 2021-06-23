f650ed78a1b6b0ae107694bde43a1cda604651f3
cool-wind-0428 <brzden32@gmail.com>
2021-06-23 12:43:59 -0300

    first commit

---
+++ precache-manifest.35cd6c97bce0b7f5ed433a4ae406c9c0.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74743dc91c13dba2a57191d9239d2f87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d9865dba71b2575101",
+    "url": "/static/css/5.d4b23571.chunk.css"
+  },
+  {
+    "revision": "aa3ef3c14cb0e48280eb",
+    "url": "/static/css/main.cb7e589a.chunk.css"
+  },
+  {
+    "revision": "7a2c46f3ecc7c06de7a2",
+    "url": "/static/js/0.bbed6887.chunk.js"
+  },
+  {
+    "revision": "f7f1da08dde408cf93a7",
+    "url": "/static/js/1.9fac8934.chunk.js"
+  },
+  {
+    "revision": "e718deee5447377bb576",
+    "url": "/static/js/10.155f3da3.chunk.js"
+  },
+  {
+    "revision": "12fbb3b0357d6632de84",
+    "url": "/static/js/11.298d39f8.chunk.js"
+  },
+  {
+    "revision": "db478938da74a33071b4",
+    "url": "/static/js/12.2fbb0fb6.chunk.js"
+  },
+  {
+    "revision": "4687927275c6718348bd",
+    "url": "/static/js/13.7e17c510.chunk.js"
+  },
+  {
+    "revision": "6f982f2f6322c65fce80",
+    "url": "/static/js/2.7332a1b5.chunk.js"
+  },
+  {
+    "revision": "10d9865dba71b2575101",
+    "url": "/static/js/5.f6d2fffc.chunk.js"
+  },
+  {
+    "revision": "9ecb41efcdaa6d9d56da52631555fbe6",
+    "url": "/static/js/5.f6d2fffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f2e312e30b0de38e86",
+    "url": "/static/js/6.c4d23068.chunk.js"
+  },
+  {
+    "revision": "fd75d8b6cd5095adbd5b",
+    "url": "/static/js/7.1cc55e00.chunk.js"
+  },
+  {
+    "revision": "08a71f4c5424376cc945",
+    "url": "/static/js/8.31bbeb23.chunk.js"
+  },
+  {
+    "revision": "b92198b9e5e59bc972b8",
+    "url": "/static/js/9.037eeaa3.chunk.js"
+  },
+  {
+    "revision": "aa3ef3c14cb0e48280eb",
+    "url": "/static/js/main.1ce739a3.chunk.js"
+  },
+  {
+    "revision": "3d51eb8dce2ca5b6092c",
+    "url": "/static/js/runtime-main.38e6ac40.js"
+  },
+  {
+    "revision": "b9e633f07681fcfa7099f1760c6e8dc7",
+    "url": "/static/media/muli-latin-200.b9e633f0.woff"
+  },
+  {
+    "revision": "c13b3c679483605b32341233e5aa4f04",
+    "url": "/static/media/muli-latin-200.c13b3c67.woff2"
+  },
+  {
+    "revision": "6fa84fa76e883a13da897f462961bfdb",
+    "url": "/static/media/muli-latin-200italic.6fa84fa7.woff2"
+  },
+  {
+    "revision": "daa58017a1f434922b590d692e6b2af7",
+    "url": "/static/media/muli-latin-200italic.daa58017.woff"
+  },
+  {
+    "revision": "e98ca76130ec6c448f108d3fa8404897",
+    "url": "/static/media/muli-latin-300.e98ca761.woff2"
+  },
+  {
+    "revision": "eccf01eadc7d7c1777ecfb36d561a0b3",
+    "url": "/static/media/muli-latin-300.eccf01ea.woff"
+  },
+  {
+    "revision": "433aeba82069b1765de9fe2e059ecaf2",
+    "url": "/static/media/muli-latin-300italic.433aeba8.woff"
+  },
+  {
+    "revision": "e2385ec378d43b5ad454c40e00103af1",
+    "url": "/static/media/muli-latin-300italic.e2385ec3.woff2"
+  },
+  {
+    "revision": "705bcc4dd1c37efca70d440041d944e8",
+    "url": "/static/media/muli-latin-400.705bcc4d.woff2"
+  },
+  {
+    "revision": "91288b87b7bbe6d6fbfb131d5dbacbf1",
+    "url": "/static/media/muli-latin-400.91288b87.woff"
+  },
+  {
+    "revision": "1e42c4d1b57bf46a137ffa2983ebdd8b",
+    "url": "/static/media/muli-latin-400italic.1e42c4d1.woff"
+  },
+  {
+    "revision": "543e731fffe3d8421819c5115a7f5141",
+    "url": "/static/media/muli-latin-400italic.543e731f.woff2"
+  },
+  {
+    "revision": "224bba330d827c890daccf803dcd5ab1",
+    "url": "/static/media/muli-latin-500.224bba33.woff"
+  },
+  {
+    "revision": "57d637872afea75f042b7d937b1db98c",
+    "url": "/static/media/muli-latin-500.57d63787.woff2"
+  },
+  {
+    "revision": "9f876a07aeab5afccedbd606fac378d7",
+    "url": "/static/media/muli-latin-500italic.9f876a07.woff2"
+  },
+  {
+    "revision": "d4c5908da6e36c6b0ecd92751bfe7ca2",
+    "url": "/static/media/muli-latin-500italic.d4c5908d.woff"
+  },
+  {
+    "revision": "5ea5ffaf259f9436753bf965e76e6e12",
+    "url": "/static/media/muli-latin-600.5ea5ffaf.woff"
+  },
+  {
+    "revision": "e840b5fe8105c3e65fda1c156335834e",
+    "url": "/static/media/muli-latin-600.e840b5fe.woff2"
+  },
+  {
+    "revision": "52b09d32a3d04d9fb5348cdb5c3fc230",
+    "url": "/static/media/muli-latin-600italic.52b09d32.woff"
+  },
+  {
+    "revision": "7db7f0f799440f2a628e36a74ab9303b",
+    "url": "/static/media/muli-latin-600italic.7db7f0f7.woff2"
+  },
+  {
+    "revision": "1d982ad1cb7695225c7c0a0cfc1b10b6",
+    "url": "/static/media/muli-latin-700.1d982ad1.woff2"
+  },
+  {
+    "revision": "c0a2a08d31879a5053d60aea0f5bb645",
+    "url": "/static/media/muli-latin-700.c0a2a08d.woff"
+  },
+  {
+    "revision": "08ea8fa20100d95518e18e84739bf109",
+    "url": "/static/media/muli-latin-700italic.08ea8fa2.woff2"
+  },
+  {
+    "revision": "4fbf29a657022481c0f868ed3ca62616",
+    "url": "/static/media/muli-latin-700italic.4fbf29a6.woff"
+  },
+  {
+    "revision": "10ca7e8e05c480f44b1be2f50cc6d025",
+    "url": "/static/media/muli-latin-800.10ca7e8e.woff"
+  },
+  {
+    "revision": "73541670fa9b16a54cd06c61c6a0a2c7",
+    "url": "/static/media/muli-latin-800.73541670.woff2"
+  },
+  {
+    "revision": "233121335c5f82380fec914d63e19221",
+    "url": "/static/media/muli-latin-800italic.23312133.woff2"
+  },
+  {
+    "revision": "6b57e7c3279568d43a93622e0e60c25f",
+    "url": "/static/media/muli-latin-800italic.6b57e7c3.woff"
+  },
+  {
+    "revision": "c805b62edeb6f97c196e3581d5c0007c",
+    "url": "/static/media/muli-latin-900.c805b62e.woff2"
+  },
+  {
+    "revision": "d84d7000d212375e4da6cb6197c01245",
+    "url": "/static/media/muli-latin-900.d84d7000.woff"
+  },
+  {
+    "revision": "14bc5b4bd274bab8694cfb91f0feb567",
+    "url": "/static/media/muli-latin-900italic.14bc5b4b.woff"
+  },
+  {
+    "revision": "fb9b3ba4a7ed8525fbde97b3f5518eb3",
+    "url": "/static/media/muli-latin-900italic.fb9b3ba4.woff2"
+  }
+]);
